f35e073c71494a9fb6e0d64224f17aa1819481ff
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-11-26 00:26:33 +0100

    added first markdown rendering

--- src/app/NoteView.tsx
+++ src/app/NoteView.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import * as showdown from "showdown"
+
 import { Typography } from 'antd';
 import { Table, Tag } from 'antd';
 
@@ -23,6 +25,27 @@ type NoteViewProps = {
   entry?: Entry,
 }
 
+
+function convertMarkdown(markdown: string): string {
+  // https://github.com/showdownjs/showdown#valid-options
+  const converter = new showdown.Converter({
+    ghCodeBlocks: true,
+    //extensions: ["highlightjs"],
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tables: true,
+    tasklists: true,
+  })
+
+  // Apparently 'github' flavor leads to treating every line break as a visible
+  // line break in the output, which is not necessarily desired if line breaks
+  // are placed in the markdown source mainly for making the source look nice,
+  // but a full paragraph is desired in the output. Disable for now.
+  // converter.setFlavor('github');
+
+  return converter.makeHtml(markdown);
+}
+
 function NoteView({ entry }: NoteViewProps) {
 
   if (entry == null) {
@@ -35,7 +58,7 @@ function NoteView({ entry }: NoteViewProps) {
     return (
       <>
         <StyledTitle>{entry.title}</StyledTitle>
-        {entry.content}
+        <div dangerouslySetInnerHTML={{ __html: convertMarkdown(entry.content!)}}/>
         <Footer/>
       </>
     );
