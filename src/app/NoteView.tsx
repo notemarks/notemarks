bcadbd7ac023f804b1a14eed461b3b625b4564ae
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-11-28 10:17:27 +0100

    added html sanitation

--- src/app/NoteView.tsx
+++ src/app/NoteView.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
-import * as showdown from "showdown"
+import showdown from "showdown";
+import DOMPurify from 'dompurify';
 
 import { Typography } from 'antd';
-import { Table, Tag } from 'antd';
 
 import styled from '@emotion/styled'
 
-import { Entry, Entries } from "./types";
-import { Repos } from "./repo";
+import { Entry } from "./types";
+
+/*
+// ------------------------------------------------------------------------------.
+// Notes:
+// ------------------------------------------------------------------------------.
+
+Setting the innerHTML from the converted markdown is an XSS vulnerability.
+There are two possibilities:
+1. Convert the HTML to pure React VDOM nodes. This is what
+   https://github.com/aknuds1/html-to-react does.
+   This may have downsides in terms of performance due to overhead?
+2. Use a sanitizer on the HTML before setting.
+   DOMPurify seems to be a popular solution.
+
+References:
+- https://stackoverflow.com/a/56394170/1804173
+- https://github.com/cure53/DOMPurify
+*/
 
 const { Title } = Typography;
 
@@ -43,7 +60,10 @@ function convertMarkdown(markdown: string): string {
   // but a full paragraph is desired in the output. Disable for now.
   // converter.setFlavor('github');
 
-  return converter.makeHtml(markdown);
+  let htmlRaw = converter.makeHtml(markdown);
+  let htmlSanitized = DOMPurify.sanitize(htmlRaw);
+
+  return htmlSanitized;
 }
 
 function NoteView({ entry }: NoteViewProps) {
@@ -58,7 +78,9 @@ function NoteView({ entry }: NoteViewProps) {
     return (
       <>
         <StyledTitle>{entry.title}</StyledTitle>
-        <div dangerouslySetInnerHTML={{ __html: convertMarkdown(entry.content!)}}/>
+        <div dangerouslySetInnerHTML={{
+          __html: convertMarkdown(entry.content || "")
+        }}/>
         <Footer/>
       </>
     );
