9a91f2caa6098e8ebde5bd4bc05a942bacd270bc
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-11-29 13:27:13 +0100

    added editor focus debug

---
+++ src/debug_editor_focus.tsx
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Editor from "@monaco-editor/react";
+import * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
+
+type IStandaloneCodeEditor = monacoEditor.editor.IStandaloneCodeEditor
+
+const EditorWrapper = () => {
+
+  const onEditorDidMount = (_: () => string, editor: IStandaloneCodeEditor) => {
+    editor.setPosition({lineNumber: 1, column: 6})
+    editor.focus()
+  }
+
+  return (
+    <Editor
+      height="90vh"
+      theme="dark"
+      language="markdown"
+      value={"Hello world"}
+      editorDidMount={onEditorDidMount}
+    />
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <EditorWrapper/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
