f506dc2d7213d85d236e61e6ac1a3b28e17057d5
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-12-05 10:41:06 +0100

    added debug tree hover

---
+++ src/debug_tree_hover.tsx
@@ -0,0 +1,53 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Tag, Tree } from 'antd';
+
+import './index.css';
+
+const TreeComponent = () => {
+  const treeData = [
+    {
+      title: "foo",
+      key: "foo",
+    },
+    {
+      title: "bar",
+      key: "bar",
+    },
+    {
+      title: "baz",
+      key: "baz",
+      children: [
+        {
+          title: "nested-foo",
+          key: "nested-foo",
+        },
+        {
+          title: "nested-bar",
+          key: "nested-bar",
+        },
+      ]
+    },
+  ]
+  return (
+    <Tree
+      treeData={treeData}
+      selectable={false}
+      titleRender={data => (
+          <Tag >
+            {data.title}
+          </Tag>
+        )
+      }
+    />
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TreeComponent/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
