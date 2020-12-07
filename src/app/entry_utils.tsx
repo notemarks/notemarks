2367cc24c835da8f3a1511d96addbc024952f54a
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-12-07 16:22:47 +0100

    improved entry sorting

--- src/app/entry_utils.tsx
+++ src/app/entry_utils.tsx
@@ -1,5 +1,5 @@
 
-import { Entries, Label, LabelCounts } from "./types"
+import { EntryKind, Entries, Label, LabelCounts } from "./types"
 import * as fn_utils from "./fn_utils"
 
 
@@ -25,9 +25,19 @@ export function getLabelCounts(entries: Entries): LabelCounts {
 }
 
 
+const entryKindNumericValues = {
+  [EntryKind.NoteMarkdown]: 0,
+  [EntryKind.Document]: 1,
+  [EntryKind.Link]: 2,
+}
+
 export function sortAndIndexEntries(entries: Entries) {
   entries.sort((a, b) => {
-    return a.title.toLowerCase().localeCompare(b.title.toLowerCase())
+    if (a.entryKind !== b.entryKind) {
+      return entryKindNumericValues[a.entryKind] - entryKindNumericValues[b.entryKind]
+    } else {
+      return a.title.toLowerCase().localeCompare(b.title.toLowerCase())
+    }
   })
   for (let i = 0; i < entries.length; ++i) {
     entries[i].idx = i;
