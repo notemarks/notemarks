45d1e0c0e96864e2b5776f9cfb7d589e1a2dd711
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-12-08 12:20:33 +0100

    added title escaping/unescaping

--- src/app/path_utils.test.tsx
+++ src/app/path_utils.test.tsx
@@ -19,7 +19,49 @@ test("filenameToTitle", () => {
   expect(filenameToTitle("foo\u29F8")).toEqual("foo/");
   expect(filenameToTitle("\u29F8foo")).toEqual("/foo");
 
-  expect(filenameToTitle("foo\u29F8\u29F8bar")).toEqual("foo\u29F8bar");
-  expect(filenameToTitle("foo\u29F8\u29F8\u29F8\u29F8bar")).toEqual("foo\u29F8\u29F8bar");
-  // expect(filenameToTitle("foo\u29F8\u29F8\u29F8bar")).toEqual("foo\u29F8/bar"); // FIXME
+  // Escaping of \u29F8
+  expect(filenameToTitle("\u29F9\u29F8")).toEqual("\u29F8");
+  expect(filenameToTitle("foo\u29F9\u29F8bar")).toEqual("foo\u29F8bar");
+  expect(filenameToTitle("\u29F9\u29F8\u29F8")).toEqual("\u29F8/");
+  expect(filenameToTitle("\u29F8\u29F9\u29F8")).toEqual("/\u29F8");
+
+  // Escaping of \u29F9
+  expect(filenameToTitle("\u29F9\u29F9")).toEqual("\u29F9");
+  expect(filenameToTitle("foo\u29F9\u29F9bar")).toEqual("foo\u29F9bar");
+  expect(filenameToTitle("\u29F9\u29F9\u29F9\u29F9")).toEqual("\u29F9\u29F9");
+
+  expect(filenameToTitle("\u29F9\u29F9\u29F8")).toEqual("\u29F9/");
+  expect(filenameToTitle("\u29F8\u29F9\u29F9")).toEqual("/\u29F9");
+
+  expect(filenameToTitle("\u29F9\u29F9\u29F9\u29F8")).toEqual("\u29F9\u29F8");
+  expect(filenameToTitle("\u29F9\u29F8\u29F9\u29F9")).toEqual("\u29F8\u29F9");
+});
+
+test("titleToFilename", () => {
+  let titleToFilename = path_utils.titleToFilename;
+  expect(titleToFilename("my_title")).toEqual("my_title");
+  expect(titleToFilename("my_title", "md")).toEqual("my_title.md");
+  expect(titleToFilename("my_title...", "md")).toEqual("my_title....md");
+
+  expect(titleToFilename("foo/bar")).toEqual("foo\u29F8bar");
+  expect(titleToFilename("foo/bar/baz")).toEqual("foo\u29F8bar\u29F8baz");
+  expect(titleToFilename("foo/")).toEqual("foo\u29F8");
+  expect(titleToFilename("/foo")).toEqual("\u29F8foo");
+
+  // Escaping of \u29F8
+  expect(titleToFilename("\u29F8")).toEqual("\u29F9\u29F8");
+  expect(titleToFilename("foo\u29F8bar")).toEqual("foo\u29F9\u29F8bar");
+  expect(titleToFilename("\u29F8/")).toEqual("\u29F9\u29F8\u29F8");
+  expect(titleToFilename("/\u29F8")).toEqual("\u29F8\u29F9\u29F8");
+
+  // Escaping of \u29F9
+  expect(titleToFilename("\u29F9")).toEqual("\u29F9\u29F9");
+  expect(titleToFilename("foo\u29F9bar")).toEqual("foo\u29F9\u29F9bar");
+  expect(titleToFilename("\u29F9\u29F9")).toEqual("\u29F9\u29F9\u29F9\u29F9");
+
+  expect(titleToFilename("\u29F9/")).toEqual("\u29F9\u29F9\u29F8");
+  expect(titleToFilename("/\u29F9")).toEqual("\u29F8\u29F9\u29F9");
+
+  expect(titleToFilename("\u29F9\u29F8")).toEqual("\u29F9\u29F9\u29F9\u29F8");
+  expect(titleToFilename("\u29F8\u29F9")).toEqual("\u29F9\u29F8\u29F9\u29F9");
 });
--- src/app/path_utils.tsx
+++ src/app/path_utils.tsx
@@ -32,21 +32,62 @@ Notes on encoding:
 In Unicode the normal `/` is U+002F SOLIDUS.
 Therefore the closest replacement is U+29F8 BIG SOLIDUS.
 
+Similarly `\` is U+005C REVERSE SOLIDUS.
+Therefore the closest replacement is U+29F9 BIG REVERSE SOLIDUS.
+
 - http://www.fileformat.info/info/unicode/char/29F8/index.htm
+- https://www.fileformat.info/info/unicode/char/29f9/index.htm
 - https://stackoverflow.com/questions/9847288/is-it-possible-to-use-in-a-filename/9847306#comment12550838_9847306
 - https://stackoverflow.com/a/61448658/1804173
 - https://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
 */
 
 function unescapeTitle(s: string): string {
+  let result = "";
+  let escaped = false;
+  for (let i = 0; i < s.length; ++i) {
+    let char = s[i];
+    if (escaped) {
+      result += char;
+      escaped = false;
+    } else {
+      if (char === "\u{29F9}") {
+        escaped = true;
+      } else if (char === "\u{29F8}") {
+        result += "/";
+      } else {
+        result += char;
+      }
+    }
+  }
+  return result;
+  // The initial implementation used a bunch of regexes.
+  // However this is awkward to get right, because each of the regexes
+  // would need an lookbehind of arbitrary length, see e.g.:
+  // https://stackoverflow.com/q/56554/1804173
+  // However, this would be tricky to reassemble with $i syntax of
+  // String.replace. So probably better to write a manual parser?
+  //
+  // The weird prefix conditions means:
+  // - either no \u{29F9} or
+  // - a double \u{29F9}\u{29F9}
+  // - or beginning of string.
+  /*
   return s
-    .replace(/([^\u{29F8}]|^)\u{29F8}([^\u{29F8}]|$)/gu, "$1/$2")
-    .replace(/\u{29F8}\u{29F8}/gu, "\u{29F8}");
+    .replace(/([^\u{29F9}]|\u{29F9}\u{29F9}|^)\u{29F8}/gu, "$1/")
+    .replace(/([^\u{29F9}]|\u{29F9}\u{29F9}|^)\u{29F9}\u{29F8}/gu, "$1\u{29F8}")
+    .replace(/\u{29F9}\u{29F9}/gu, "\u{29F9}");
+  */
+}
+
+function escapeTitle(s: string): string {
+  return s
+    .replace(/\u{29F9}/gu, "\u{29F9}\u{29F9}")
+    .replace(/\u{29F8}/gu, "\u{29F9}\u{29F8}")
+    .replace(/\//gu, "\u{29F8}");
 }
 
 export function filenameToTitle(filename: string) {
-  // TODO: Unescaping of special chars has to go here...
-
   let idxLastDot = filename.lastIndexOf(".");
   if (idxLastDot === -1) {
     return unescapeTitle(filename);
@@ -56,8 +97,7 @@ export function filenameToTitle(filename: string) {
 }
 
 export function titleToFilename(title: string, extension?: string) {
-  // TODO: Escaping of special chars has to go here...
-  let titleEscaped = title;
+  let titleEscaped = escapeTitle(title);
   if (extension != null && extension.length > 0) {
     return `${titleEscaped}.${extension}`;
   } else {
